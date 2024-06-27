82bdfbd726a8a8fce6dbdd8242b898620133dc95
ju52les <nils.alexander.haupt@uni-jena.de>
2024-06-27 23:40:44 +0200

    FingerblastOptions: small fix in description of RankWithEpimetheus option

--- sirius_cli/src/main/java/de/unijena/bioinf/ms/frontend/subtools/fingerblast/FingerblastOptions.java
+++ sirius_cli/src/main/java/de/unijena/bioinf/ms/frontend/subtools/fingerblast/FingerblastOptions.java
@@ -73,7 +73,7 @@ public class FingerblastOptions implements ToolChainOptions<FingerblastSubToolJo
         defaultConfigOptions.changeOption("ExpansiveSearchConfidenceMode.confidenceScoreSimilarityMode", expansiveSearchConfMode);
     }
 
-    @Option(names = {"-e, --epi"}, descriptionKey = "RankWithEpimetheus", description = {"Use the combinatorial fragmenter, Epimetheus, to rank the structure candidates."})
+    @Option(names = {"-e, --epi"}, descriptionKey = "RankWithEpimetheus", description = {"Use the combinatorial fragmenter, EPIMETHEUS, to rank the structure candidates."})
     public void setRankWithEpimetheus(DefaultParameter value) throws Exception {
         defaultConfigOptions.changeOption("RankWithEpimetheus", value);
     }
